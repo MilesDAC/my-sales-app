ffcb76be6119428c7c48e02abf15ff8a211fad50
Milo Collins <cmiles0710@gmail.com>
2022-10-28 12:50:59 +0100

    6.4 : suppliers service and base componenets + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { CategoriesLoadingAsyncComponent } from './categories/categories-loading-async/categories-loading-async.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SuppliersListComponent } from './suppliers/suppliers-list/suppliers-list.component';
+import { SuppliersComponent } from './suppliers/suppliers.component';
 
 const routes: Routes = [
   {path:'', component:DashboardComponent},
   {path:'categories', component:CategoriesComponent},
   {path:'categories-async', component: CategoriesLoadingAsyncComponent},
+  {
+    path: 'suppliers',
+    component: SuppliersComponent,
+    children: [
+      {
+        path: '',
+        component: SuppliersListComponent
+      },
+    ]
+  },
+  { path: '', component: DashboardComponent }
 ]
 
 @NgModule({
